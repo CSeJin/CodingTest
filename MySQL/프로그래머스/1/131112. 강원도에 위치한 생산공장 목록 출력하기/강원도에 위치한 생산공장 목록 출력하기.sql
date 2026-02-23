2e7dea4b178b522efb12aa08a616b3306560d59e
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-23 20:05:13 +0900

    Enhance SQL query with wildcard usage comments
    Added comments explaining the use of SQL wildcards.

--- MySQL/프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
+++ MySQL/프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
@@ -2,3 +2,13 @@
 SELECT factory_id, factory_name, address
 from food_factory
 where address like "강원도%" ;
+
+-- like 와일드 카드를 사용한다.
+
+-- %: 0개 이상의 문자(null은 제외)
+
+-- _: 한 개의 문자
+
+-- []: 대괄호 내 문자 중 하나 이상 존재하는 컬럼 반환
+
+-- [^ ]: 대괄호 내 문자가 없는 컬럼 반환
