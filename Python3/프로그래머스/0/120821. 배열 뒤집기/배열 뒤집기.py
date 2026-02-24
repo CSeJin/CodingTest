f558fcafd4e268871044c6b6598d8d6709aeac6c
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-24 14:07:38 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ Python3/프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -4,3 +4,19 @@ def solution(num_list):
     for i in range(len(num_list)-1, -1, -1):
         answer.append(num_list[i])
     return answer
+
+# 방법 2
+def solution(num_list):
+    return num_list[::-1]
+
+# 방법 3
+def solution(num_list):
+    answer =[]
+    while(num_list):
+        answer.append(num_list.pop())
+    return answer
+
+# 방법 4
+def solution(num_list):
+    num_list.reverse()  # reverse()는 리턴이 null
+    return num_list
