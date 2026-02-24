c0fc5b29ad710bd2bb581bc15df271450b47a663
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-24 13:44:15 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 9.09 MB -BaekjoonHub

--- Python3/프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ Python3/프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,9 +1,17 @@
+# 방법 1
 def solution(n):
     if n <=0 or n > 1000:
         print("제한사항 오류")
         return
     answer = 0
-    for i in range(n+1):
-        if i % 2 == 0:
-            answer += i
+    for i in range(2, n+1, 2):
+        answer += i
     return answer
+
+# 방법 2
+def solution(n):
+    return 2*(n//2)*((n//2)+1)/2
+
+# 방법 3
+def solution(n):
+    return sum(i for i in range(2, n+1, 2))
