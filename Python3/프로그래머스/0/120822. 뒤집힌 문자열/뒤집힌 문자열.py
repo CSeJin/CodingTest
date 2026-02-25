cda943d2b99ba1dde19ad25d0944f05b2f563528
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-25 14:45:07 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- Python3/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.py
+++ Python3/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.py
@@ -1,3 +1,16 @@
 # 방법 1
 def solution(my_string):
     return  my_string[::-1]
+
+# 방법 2
+def solution(my_string):
+    return  ''.join(reversed(my_string))
+
+# 방법 1
+def solution(my_string):
+    answer = ''
+    
+    for i in range(len(my_string)-1, -1, -1):
+        answer += my_string[i]
+    
+    return  answer
