0985602f271216b32cb60a29071eeffe54b2d144
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-25 15:20:42 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- Python3/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ Python3/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,6 +1,12 @@
+# 방법 1
 def solution(numbers):
     a = max(numbers)
     numbers.pop(numbers.index(a))
     b = max(numbers)
     
     return a*b
+
+# 방법 2
+def solution(numbers):
+    numbers.sort()
+    return numbers[-1] * numbers[-2]
