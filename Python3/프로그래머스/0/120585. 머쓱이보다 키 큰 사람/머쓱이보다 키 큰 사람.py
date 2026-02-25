29dc424a5cfa71ac9ea193902029cba0212504e1
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-25 15:31:05 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ Python3/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -8,5 +8,11 @@ def solution(array, height):
     return answer
 
 # 방법 2
-#def solution(array, height):
-    
+def solution(array, height):
+    return sum(1 for x in array if x > height)
+
+# 방법 3
+def solution(array, height):
+    array.append(height)
+    array.sort(reverse=True)
+    return array.index(height)
