4c92ac71883447931440dd541dc1691ba6d37cf5
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-26 17:37:54 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ Python3/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,6 +1,11 @@
+# 방법 1
 def solution(sides):
     sides.sort()
     longest = sides.pop()
     if longest >= sum(sides):
         return 2
     return 1
+
+# 방법 2
+def solution(sides):
+    return 1 if max(sides) < (sum(sides)-max(sides)) else 2
