9f6a0766f131d9968f70dcdbb1101065a9fbb503
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-27 15:24:29 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 9.34 MB -BaekjoonHub

--- Python3/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ Python3/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,3 +1,7 @@
+# 방법 1
 def solution(slice, n):
-    return n//slice + 1 if n%slice!=0 else n//slice
-    
+    return n//slice + (1 if n%slice!=0 else 0)
+
+# 방법 2
+def solution(slice, n):
+    return (n-1)//slice +1
