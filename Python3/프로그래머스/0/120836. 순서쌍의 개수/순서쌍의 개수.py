a983920bf9ac229cb7778290d5ffb46d752bb34b
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-02-27 16:07:18 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.14 ms, Memory: 9.34 MB -BaekjoonHub

--- Python3/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ Python3/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -5,6 +5,15 @@ def solution(n):
 
 # 방법 2
 def solution(n):
-    list = [i for i in range(1, n+1)]
-    return sum(1 for i in list if n % i ==0)
+    return sum(1 for i in range(1, n+1) if n % i ==0)
+
+# 방법 3
+def solution(n):
+    answer = 0
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            answer +=2
+            if i*i == n:
+                answer -= 1
+    return answer
     
