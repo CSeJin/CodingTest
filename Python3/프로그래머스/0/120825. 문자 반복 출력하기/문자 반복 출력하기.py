9cbce10ba9dd96a8098c2688f7714bff917e7e8d
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-01 17:20:15 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- Python3/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ Python3/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -4,3 +4,7 @@ def solution(my_string, n):
     for c in my_string:
         repeated_string += c*n
     return repeated_string
+
+# 방법 2
+def solution(my_string, n):
+    return ''.join(c*n for c in my_string)
