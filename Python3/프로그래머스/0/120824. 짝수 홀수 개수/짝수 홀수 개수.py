71ce1404be88eb97e25288b4a0fe987be8c5c2ca
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-02 16:56:13 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.36 MB -BaekjoonHub

--- Python3/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ Python3/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -8,3 +8,14 @@ def solution(num_list):
             even_odd[1] += 1
     return even_odd
     
+# 방법 2
+def solution(num_list):
+    odd_num = sum(1 for num in num_list if num%2 != 0)
+    return [len(num_list)-odd_num, odd_num]
+
+# 방법 3
+def solution(num_list):
+    li = [0,0]
+    for num in num_list:
+        li[num%2] += 1
+    return li
