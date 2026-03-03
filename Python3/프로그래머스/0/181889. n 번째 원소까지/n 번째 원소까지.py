b7da3d20b154dd98443870300a90758a5b772759
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-03 16:25:21 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.00 ms, Memory: 9.36 MB -BaekjoonHub

--- Python3/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.py
+++ Python3/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.py
@@ -1,3 +1,8 @@
+# 방법 1
 def solution(num_list, n):
-    return[num_list[i] for i in range(n)]
+    return [num_list[i] for i in range(n)]
+
+# 방법 2
+def solution(num_list, n):
+    return num_list[:n]
         
