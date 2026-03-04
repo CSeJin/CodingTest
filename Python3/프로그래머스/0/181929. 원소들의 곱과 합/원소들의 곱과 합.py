abd4676096d5d984891f89822dd96021e86e7935
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-04 12:28:11 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 9.24 MB -BaekjoonHub

--- Python3/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ Python3/프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,6 +1,13 @@
+# 방법 1
 def solution(num_list):
     mult, square = 1,0
     for i in num_list:
         mult *= i
     square = sum(i for i in num_list)**2
     return 1 if mult < square else 0
+
+# 방법 2
+def solution(num_list):
+    s = sum(num_list)**2
+    m = eval('*'.join(str(i) for i in num_list))
+    return 1 if s > m else 0
