6c7e1704a0b37aef8ecb35810789f60814f27fd1
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-05 17:44:18 +0900

    [level 0] Title: 수 조작하기 1, Time: 5.94 ms, Memory: 9.36 MB -BaekjoonHub

--- Python3/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ Python3/프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,5 +1,11 @@
+# 방법 1
 def solution(n, control):
     dict = {'w':1, 's':-1, 'd':10, 'a':-10}
     for c in control:
         n += dict[c]
     return n
+
+# 방법 2
+def solution(n, control):
+    key = dict(zip(['w','s','d','a'], [1,-1,10,-10]))
+    return n + sum(key[c] for c in control)
