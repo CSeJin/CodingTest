1365b23bb82def96f8660d9c4fc45c00732c4941
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-07 10:33:03 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 12.07 ms, Memory: 7.29 MB -BaekjoonHub

--- Python3/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.py
+++ Python3/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.py
@@ -1,3 +1,11 @@
-input_str = input()
-idx = input_str.index(' ')
-print(input_str[:idx]+input_str[idx+1:])
+# 방법 1
+# input_str = input()
+# idx = input_str.index(' ')
+# print(input_str[:idx]+input_str[idx+1:])
+
+# 방법 2
+# print(input().strip().replace(' ',''))
+
+# 방법 3
+str1, str2 = input().strip().split()
+print(str1, str2, sep='')
