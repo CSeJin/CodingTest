b4a1404bee674545cd628126e3e51a36ba19d674
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-08 12:02:13 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 9.31 MB -BaekjoonHub

--- Python3/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ Python3/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,5 +1,10 @@
+# 방법 1
 def solution(a, b):
     ab = int(str(a)+str(b))
     ba = int(str(b)+str(a))
     
     return max(ab, ba)
+
+# 방법 1
+def solution(a, b):
+    return max(int(f"{a}{b}"), int(f"{b}{a}"))
