04030108d938076fe04e5699f1017fc94ed5a88c
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-11 11:24:51 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.04 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/0/120911.문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ Python3/프로그래머스/0/120911.문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,2 +1,13 @@
+# 방법 1
 def solution(my_string):
     return ''.join(sorted(my_string.lower()))
+
+# 방법 2
+def solution(my_string):
+    answer = []
+    for s in my_string:
+        if ord(s) >= ord('A') and ord(s) <= ord('Z'):
+            answer.append(chr(ord(s)+32))
+        else:
+            answer.append(s)
+    return ''.join(sorted(answer))
