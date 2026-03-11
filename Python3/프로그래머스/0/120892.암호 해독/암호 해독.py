b1751cf4ebd4d1b7151ad5fa408e6b3e3cfa428d
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-11 11:07:35 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- Python3/프로그래머스/0/120892.암호 해독/암호 해독.py
+++ Python3/프로그래머스/0/120892.암호 해독/암호 해독.py
@@ -1,2 +1,7 @@
+# 방법 1
 def solution(cipher, code):
-    return ''.join(cipher[c] for c in range(code-1, len(cipher), code)) 
+    return ''.join(cipher[c] for c in range(code-1, len(cipher), code))
+
+# 방법 2
+def solution(cipher, code):
+    return cipher[code-1::code]
