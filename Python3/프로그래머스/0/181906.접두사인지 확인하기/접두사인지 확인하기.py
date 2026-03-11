0d52d9a3cacd2aa222987fe15ff61a6bfdeed529
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-11 11:32:40 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- Python3/프로그래머스/0/181906.접두사인지 확인하기/접두사인지 확인하기.py
+++ Python3/프로그래머스/0/181906.접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(my_string, is_prefix):
     if len(is_prefix) > len(my_string):
         return 0
@@ -5,3 +6,13 @@ def solution(my_string, is_prefix):
         if my_string[i]!=is_prefix[i]:
             return 0
     return 1
+
+# 방법 2
+def solution(my_string, is_prefix):
+    return int(my_string.startswith(is_prefix))
+
+# 방법 3
+def solution(my_string, is_prefix):
+    if my_string[:len(is_prefix)] == is_prefix:
+        return 1
+    return 0
