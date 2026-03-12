fca36f9ec27e5bcdc62f6d40619d587dac13f0d9
Sejin Choi <127668461+CSeJin@users.noreply.github.com>
2026-03-12 11:29:11 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/3/293261.물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,9 @@
+select i.id, n.fish_name, i.length
+from fish_info i
+join fish_name_info n on i.fish_type = n.fish_type
+where (i.length, i.fish_type) in (
+    select max(length) as length, fish_type
+    from fish_info
+    group by fish_type
+    )
+order by i.id asc;
